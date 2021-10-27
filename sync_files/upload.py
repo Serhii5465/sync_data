6f093748f941d5f830a8e4140259aeb6fd2d4c7e
raisnet <raisnet213@gmail.com>
2021-10-27 13:49:12 +0300

    fix path syncing folders

--- sync_files/upload.py
+++ sync_files/upload.py
@@ -4,11 +4,19 @@ import sys
 
 def upload_files(command, list_sync_dirs, uuid_recv_disk, logs_dir, test_mode):
     for i in list_sync_dirs:
-        command[len(command) - 2] = i
 
-        name_sync_dir = os.path.basename(os.path.dirname(i))    # get name sync folder from full path
-                                                                # arg: /d/projects/bash-scripts/sync_files/
-                                                                # result: sync_files
+        #normalize path 
+        # arg: /d/projects/bash-scripts/sync_files/
+        # result: /d/projects/bash-scripts/sync_files
+        norm_path = os.path.normpath(i) 
+
+        command[len(command) - 2] = norm_path
+
+        # get name sync folder from full path
+        # arg: /d/projects/bash-scripts/sync_files
+        # result: sync_files
+        name_sync_dir = os.path.basename(norm_path)  
+
         if (test_mode):
             command[len(command) - 3] = '--log-file=' + logs_dir + '/' + uuid_recv_disk + '__TEST_MODE__' + name_sync_dir + '.txt'
         else:
