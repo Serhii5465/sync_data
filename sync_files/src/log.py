ed0e7daf9f163f44766b37bffa42607d40e5b872
raisnet <raisnet213@gmail.com>
2021-11-17 00:41:08 +0200

    adding time to path logs dir

--- sync_files/src/log.py
+++ sync_files/src/log.py
@@ -1,9 +1,11 @@
 from pathlib import Path
+import src.time as time
 
-def get_logs_dir(name):
+def get_logs_dir(name_dir):
     home_dir = str(Path.home())
+    date_now = time.get_time_now()
 
-    full_path_logs_dir = home_dir + '/logs/' + name
+    full_path_logs_dir = home_dir + '/logs/' + name_dir + '/' + date_now
 
     Path(full_path_logs_dir).mkdir(parents=True, exist_ok=True)
 
