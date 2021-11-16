9733cd92e81dcdc051b933f5f203a5d3473a8d54
raisnet <raisnet213@gmail.com>
2021-11-17 00:39:34 +0200

    rename file backuping docs

---
+++ sync_files/backup_docs.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+from time import gmtime, strftime
+import src.log as log
+import src.bash_proc as bash_proc
+import src.time as time
+import sys
+import os
+
+
+def check_files(*args):
+    for i in args:
+        if (not os.path.exists(i)):
+            msg = i + " doesn't exists"
+            sys.exit(msg)
+            
+
+def upload_to_gdrive(rclone_dir, sync_dir, logs_dir):
+    win_style_path_sync_dir = bash_proc.get_cmd_output(['cygpath', '--windows', sync_dir])
+    win_style_path_logs_dir = bash_proc.get_cmd_output(['cygpath', '--windows', logs_dir])
+
+    date = time.get_time_now()
+
+    out = bash_proc.run_cmd([rclone_dir + '/rclone.exe', 
+                        'sync', 
+                        '--progress', 
+                        '--verbose',
+                        win_style_path_sync_dir.stdout.strip('\n'), 
+                        'google-drive:',
+                        '--log-file=' + win_style_path_logs_dir.stdout.strip('\n') + '\\' + date + '.txt'])
+    
+    
+    if (out.returncode != 0):
+        sys.exit('Error\nCheck logs')
+    else:
+        print('\nGood')
+
+
+def main():
+    logs_dir = log.get_logs_dir('rclone')
+
+    rclone_config = str(Path.home()) + '/.config/rclone/rclone.conf'
+    rclone_path_dir = '/cygdrive/c/portable/rclone'
+    sync_dir = '/cygdrive/d/documents'
+
+    check_files(rclone_config, rclone_path_dir, sync_dir)
+    
+    upload_to_gdrive(rclone_path_dir, sync_dir, logs_dir)
+
+
+main()
