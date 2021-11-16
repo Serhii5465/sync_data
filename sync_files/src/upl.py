ca799e849c71694488bf98a2630efaf57b310c5d
raisnet <raisnet213@gmail.com>
2021-11-17 00:40:38 +0200

    fix

--- sync_files/src/upl.py
+++ sync_files/src/upl.py
@@ -4,14 +4,14 @@ import os
 import sys
 
 # insert into rsync command arguments of source and path log file
-def inst_log_arg(command, is_test_mode, path_logs_dir, uuid_drive, name_sync_dir):
+def inst_log_arg(command, is_test_mode, path_logs_dir, name_model_recv_drive, name_sync_dir):
     if (is_test_mode):
-        command[len(command) - 3] = '--log-file=' + path_logs_dir + '/' + uuid_drive + '__TEST_MODE__' + name_sync_dir + '.txt'
+        command[len(command) - 3] = '--log-file=' + path_logs_dir + '/' + name_model_recv_drive + '__' + name_sync_dir + '_TEST_MODE' +'.txt'
     else:
-        command[len(command) - 3] = '--log-file=' + path_logs_dir + '/' + uuid_drive + '__' + name_sync_dir + '.txt'
+        command[len(command) - 3] = '--log-file=' + path_logs_dir + '/' + name_model_recv_drive + '__' + name_sync_dir + '.txt'
 
 
-def upload_files(command, list_sync_dirs, uuid_disk, path_logs_dir, is_test_mode):
+def upload_files(command, list_sync_dirs, name_model_recv_drive, path_logs_dir, is_test_mode):
     for dir in list_sync_dirs:
         # set argument source of sync
         command[len(command) - 2] = dir
@@ -20,7 +20,7 @@ def upload_files(command, list_sync_dirs, uuid_disk, path_logs_dir, is_test_mode
         # arg: /d/projects/bash-scripts/sync_files
         # result: sync_files
         name_sync_dir = os.path.basename(dir)  
-        inst_log_arg(command, is_test_mode, path_logs_dir, uuid_disk, name_sync_dir)
+        inst_log_arg(command, is_test_mode, path_logs_dir, name_model_recv_drive, name_sync_dir)
 
         code = bash_proc.run_cmd(command)
 
@@ -28,7 +28,7 @@ def upload_files(command, list_sync_dirs, uuid_disk, path_logs_dir, is_test_mode
            sys.exit('Error\nCheck logs')
 
 
-def upload_vdi(command1, command2, full_pth_src_dir, root_pth_src_drive, full_path_dest_dir, uuid_disk, path_logs_dir, is_test_mode):
+def upload_vdi(command1, command2, full_pth_src_dir, root_pth_src_drive, full_path_dest_dir, name_model_recv_drive, path_logs_dir, is_test_mode):
     list_vdi_img = glob.glob(full_pth_src_dir + '/**/*.vdi', recursive=True) # search .vdi files
 
     name_dir_vdi = full_pth_src_dir.split(root_pth_src_drive)[1].strip('/') # output: /VirtualBox_VMs
@@ -46,12 +46,12 @@ def upload_vdi(command1, command2, full_pth_src_dir, root_pth_src_drive, full_pa
         if (not os.path.isfile(path_dest_vdi)):
             command1[len(command1) - 2] = i
             command1[len(command1) - 1] = path_dest_vdi
-            inst_log_arg(command1, is_test_mode, path_logs_dir, uuid_disk, name_dir_vdi)
+            inst_log_arg(command1, is_test_mode, path_logs_dir, name_model_recv_drive, name_dir_vdi)
             out = bash_proc.run_cmd(command1)
         else:
             command2[len(command2) - 2] = i
             command2[len(command2) - 1] = path_dest_vdi
-            inst_log_arg(command2, is_test_mode, path_logs_dir, uuid_disk, name_dir_vdi)
+            inst_log_arg(command2, is_test_mode, path_logs_dir, name_model_recv_drive, name_dir_vdi)
             out = bash_proc.run_cmd(command2)
 
         if (out.returncode != 0):
