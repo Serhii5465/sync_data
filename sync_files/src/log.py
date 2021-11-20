8581e1f6c61de37c973500320503ae4c262fcf8e
raisnet <raisnet213@gmail.com>
2021-11-20 17:34:28 +0200

    fix

--- sync_files/src/log.py
+++ sync_files/src/log.py
@@ -7,6 +7,6 @@ def get_logs_dir(name_dir):
 
     full_path_logs_dir = '/cygdrive/d/logs/' + name_dir + '/' + date_now
 
-    Path(os.path.dirname(full_path_logs_dir)).mkdir(parents=True, exist_ok=True)
+    Path(full_path_logs_dir).mkdir(parents=True, exist_ok=True)
 
     return full_path_logs_dir
