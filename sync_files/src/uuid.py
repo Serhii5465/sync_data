6042adf6b2df892f64cafa448d953b24cc6b8c92
raisnet <raisnet213@gmail.com>
2022-02-07 00:47:01 +0200

    init commit

---
+++ sync_files/src/uuid.py
@@ -0,0 +1,11 @@
+# Wester Digital 1Tb
+uuid_wd_drive = '0016381016380964'
+
+# Hitahci 500Gb
+uuid_hitachi_drive = 'BE6233E062339C59'
+
+# Dell Inspiron 15 3576 (Partition D:)
+uuid_dell_3576_drive = '01D7DEEE65713660'
+
+# 2.5″ Western Digital 320Gb
+uuid_jmicron_drive = 'B880237780233B70'
