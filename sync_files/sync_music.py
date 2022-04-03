34595c2e7053dbbfefa813f469a96bf9d4d45eb3
raisnet <raisnet213@gmail.com>
2022-04-03 12:19:26 +0300

    fixing bug re-uploading files

--- sync_files/sync_music.py
+++ sync_files/sync_music.py
@@ -1,3 +1,4 @@
+import os
 from src import bash_proc
 
 def get_out_adb_cmd(arg):
@@ -7,27 +8,27 @@ def get_out_adb_cmd(arg):
 def run_adb_cmd(command):
     bash_proc.run_cmd(command)
 
-def upload(src, dest):
+def upload(src, dest, root_dest):
     arg_cmd_exist_dir = '[ -d ' + dest + ' ] && echo 0 || echo 1'  # 0 - exist, 1 - not exist 
     arg_cmd_empty_dir = 'find ' + dest + ' -mindepth 1 -maxdepth 1 | read && echo 0 || echo 1' # 0 - not empty, 1 - empty 
 
     is_exist = get_out_adb_cmd(arg_cmd_exist_dir)
     is_empty = get_out_adb_cmd(arg_cmd_empty_dir)
 
-    cmd_adb_push = ['adb', 'push', src, dest]
-    cmd_adb_sync = ['adb', 'push', '--sync', src, dest]
+    cmd_adb_push = ['adb', 'push', src, root_dest]
+    cmd_adb_sync = ['adb', 'push', '--sync', src, root_dest]
 
     if is_exist == '1' and is_empty == '1':
         # adb push
-        print('Directory Music not exist')
+        print('not exist')
         run_adb_cmd(cmd_adb_push)
     if is_exist == '0' and is_empty == '1':
         # adb push
-        print('Directory Music exist,empty')
+        print('exist,empty')
         run_adb_cmd(cmd_adb_push)
     if is_exist == '0' and is_empty == '0':
         # adb push --sync
-        print('Directory Music exist,not empty')
+        print('exist,not empty')
         run_adb_cmd(cmd_adb_sync)
 
 
@@ -36,6 +37,7 @@ def main():
     win_src_dir = bash_proc.get_cmd_output(['cygpath.exe', '--windows', unix_src_dir]).stdout.strip('\n')
     
     dest_dir = '/storage/self/primary/Music'
-    upload(win_src_dir, dest_dir)
+    root_dest_dir = os.path.dirname(dest_dir)
+    upload(win_src_dir, dest_dir, root_dest_dir)
 
 main()
