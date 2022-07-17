1e19452a2d731368fa9ca258f815985bc4c15b95
raisnet <raisnet213@gmail.com>
2022-07-17 21:37:43 +0300

    changing uuid

--- sync_files/src/uuid.py
+++ sync_files/src/uuid.py
@@ -2,7 +2,7 @@
 uuid_wd_drive = '0016381016380964'
 
 # Hitahci 500Gb
-uuid_hitachi_drive = 'BE6233E062339C59'
+uuid_hitachi_drive = 'BE102871102832B7'
 
 # Dell Inspiron 15 3576
 uuid_dell_3576_drive = '01D7DEEE65713660'
