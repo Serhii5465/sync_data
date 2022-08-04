355932446afc6e55000c8a9fb6c2ac68269210e3
raisnet <raisnet213@gmail.com>
2022-08-04 23:42:40 +0300

    Added logging of downloadable files.

--- sync_files/sync_music.py
+++ sync_files/sync_music.py
@@ -41,59 +41,104 @@ def upload(win_src_dir, root_unix_src_dir, root_dest_dir, sync_dir):
     cmd_adb_push = ['adb', 'push', win_src_dir, root_dest_dir]
     cmd_adb_sync = ['adb', 'push', '--sync', win_src_dir, root_dest_dir]
 
+    # Initialization of logger
+    logger = get_logger()
+
     if is_exist == '1' and is_empty == '1':
         # adb push
         print('Music folder not exist')
+        log_stat_files(get_diff_files(root_dest_dir, sync_dir, root_unix_src_dir), logger, 'Downloadable file: ')
         bash_proc.run_cmd(cmd_adb_push)
 
     if is_exist == '0' and is_empty == '1':
         # adb push
         print('Music folder exist,but empty')
+        log_stat_files(get_diff_files(root_dest_dir, sync_dir, root_unix_src_dir), logger, 'Downloadable file: ')
         bash_proc.run_cmd(cmd_adb_push)
 
     if is_exist == '0' and is_empty == '0':
         # adb push --sync
         print('Music folder exist and not empty')
 
-        # Getting list of files on Android device
-        cmd_adb_get_rem_files = ['adb', 'shell', 'cd ' + root_dest_dir + ' && find ' + sync_dir + ' -type f']
-        list_rem_files = get_form_out_cmd(cmd_adb_get_rem_files).split('\n')
+        is_upload = False
+        delete_remt_files(get_diff_files(root_dest_dir, sync_dir, root_unix_src_dir, is_upload),
+                          root_dest_dir,
+                          sync_dir,
+                          logger)
 
-        # Getting list of files on PC
-        os.chdir(root_unix_src_dir)
-        cmd_get_loc_files = ['find', sync_dir, '-type', 'f']
-        list_loc_files = get_form_out_cmd(cmd_get_loc_files).split('\n')
-
-        # List of full paths to files, which are missing on Android
-        list_miss_files = ['"' + root_dest_dir + i + '"' for i in list(set(list_rem_files) - set(list_loc_files))]
-
-        if len(list_miss_files) != 0:
-            # Initialization of logger
-            logger = get_logger()
-
-            cmd_del_file = ['adb', 'shell', 'rm', '']
-            print('Removing files')
-            for i in list_miss_files:
-                print(i)
-                logger.info('Deletable file: ' + i)
-                cmd_del_file[len(cmd_del_file) - 1] = i
-                out = bash_proc.get_cmd_output(cmd_del_file)
-                if out.returncode != 0:
-                    print(out.stderr)
-                    logger.error(out.stderr)
-
-            # Utility 'find' removing empty folders
-            cmd_del_empt_dir = ['adb', 'shell', 'find ' + root_dest_dir + sync_dir + ' -type d -delete']
-            str_empty_dirs = get_form_out_cmd(cmd_del_empt_dir)
-            if str_empty_dirs != '':
-                print('Removing empty directories')
-                list_empty_dirs = str_empty_dirs.split('\n')
-                for i in list_empty_dirs:
-                    print(i)
-                    logger.info('Deletable directory: ' + i)
+        log_stat_files(get_diff_files(root_dest_dir, sync_dir, root_unix_src_dir), logger, 'Downloadable file: ')
 
         print('Uploading files')
         bash_proc.run_cmd(cmd_adb_sync)
+
+
+def delete_remt_files(list_miss_files, root_dest_dir, sync_dir, logger):
+    """
+    Deleting missing files on the local PC from the Android.
+    @param list_miss_files: An array of files that are missing on the sender side.
+    @param root_dest_dir: Full path to root-folder, where is located folder-receiver.
+    @param sync_dir: Name of folder-transmitter.
+    @param logger: Instance of logger.
+    """
+    if len(list_miss_files) != 0:
+        cmd_del_file = ['adb', 'shell', 'rm', '']
+        print('Removing files')
+        for i in list_miss_files:
+            print(i)
+            logger.info('Deletable file: ' + i)
+            cmd_del_file[len(cmd_del_file) - 1] = i
+            out = bash_proc.get_cmd_output(cmd_del_file)
+            if out.returncode != 0:
+                print(out.stderr)
+                logger.error(out.stderr)
+
+        # Utility 'find' removing empty folders
+        cmd_del_empt_dir = ['adb', 'shell', 'find ' + root_dest_dir + sync_dir + ' -type d -delete']
+        str_empty_dirs = get_form_out_cmd(cmd_del_empt_dir)
+        if str_empty_dirs != '':
+            print('Removing empty directories')
+            list_empty_dirs = str_empty_dirs.split('\n')
+            for i in list_empty_dirs:
+                print(i)
+                logger.info('Deletable directory: ' + i)
+
+
+def get_diff_files(root_dest_dir, sync_dir, root_unix_src_dir, is_upload=True):
+    """
+    Finds and compares files on local PC and Android device for further download (update) or deletion.
+    @param root_dest_dir: Full path to root-folder, where is located folder-receiver.
+    @param sync_dir: Name of folder-transmitter.
+    @param root_unix_src_dir: Full path to root-folder in UNIX style format, where is located folder-transmitter
+    @param is_upload: Indicates, which array will be returned. If true, function returned list of files, which
+    be uploaded. Otherwise, returns list of files, which will be removed from Android device.
+    @return:
+    """
+    # Getting list of files on Android device
+    cmd_adb_get_rem_files = ['adb', 'shell', 'cd ' + root_dest_dir + ' && find ' + sync_dir + ' -type f']
+    list_rem_files = get_form_out_cmd(cmd_adb_get_rem_files).split('\n')
+
+    # Getting list of files on PC
+    os.chdir(root_unix_src_dir)
+    cmd_get_loc_files = ['find', sync_dir, '-type', 'f']
+    list_loc_files = get_form_out_cmd(cmd_get_loc_files).split('\n')
+
+    if not is_upload:
+        # List of full paths to files, which are missing on Android
+        return ['"' + root_dest_dir + i + '"' for i in list(set(list_rem_files) - set(list_loc_files))]
+    else:
+        # List of full paths to files, which are will be uploaded from PC to Android device
+        return ['"' + root_unix_src_dir + i + '"' for i in list(set(list_loc_files) - set(list_rem_files))]
+
+
+def log_stat_files(list_upl_files, logger, msg):
+    """
+    The function logs the full path to the file, which will be uploaded.
+    @param list_upl_files: List of uploading files.
+    @param logger: Instance of logger.
+    @param msg: Message status of file.
+    """
+    for i in list_upl_files:
+        logger.info(msg + i)
 
 
 def get_stat_dev():
