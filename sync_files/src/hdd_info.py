fed27f5b3654861cfe98c68299f47054c36e06dc
raisnet <raisnet213@gmail.com>
2022-08-31 10:18:52 +0300

    Recycling hard drive storage information

---
+++ sync_files/src/hdd_info.py
@@ -0,0 +1,22 @@
+# Wester Digital 1Tb (Maiwo External Enclosure)
+wd_drive = {
+    "uuid" : '004AB8894AB87D4A',
+    "name" : 'Wester Digital (Maiwo)'
+}
+
+# Hitachi 500Gb (Maiwo External Enclosure)
+hitachi_drive = {
+    "uuid" : 'BE102871102832B7',
+    "name" : 'Hitachi (Maiwo)'
+}
+
+# Wester Digital 1Tb (Dell Inspiron 15 3576)
+dell_3576_drive = {
+    "uuid" : 'CE0E2EF20E2ED36F'
+}
+
+# 2.5″ Western Digital 320Gb (JMicron)
+jmicron_drive = {
+    "uuid" : '01D88D3C9AF9DC90',
+    "name" : 'Western Digital (JMicron)'
+}
