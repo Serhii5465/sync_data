bee1d9d75268067caa8a3baf972fde6c55a3560c
raisnet <raisnet213@gmail.com>
2022-09-17 20:04:27 +0300

    Reworking methods of storing data

--- sync_files/src/data/hdd_info.py
+++ sync_files/src/data/hdd_info.py
@@ -1,41 +1,32 @@
 class HddInfo:
 
-    def __init__(self):
+    @staticmethod
+    def wd_drive():
         # Wester Digital 1Tb (Maiwo External Enclosure)
-        self.__wd_drive = {
+        return {
             'uuid' : '088C11A28C118AF8',
             'name' : 'Wester Digital (Maiwo)'
         }
 
+    @staticmethod
+    def hitachi_drive():
         # Hitachi 500Gb (Maiwo External Enclosure)
-        self.__hitachi_drive = {
+        return {
             'uuid': '74FAE577FAE53650',
             'name': 'Hitachi (Maiwo)'
         }
 
+    @staticmethod
+    def dell_3576_drive():
         # Wester Digital 1Tb (Dell Inspiron 15 3576)
-        self.__dell_3576_drive = {
+        return {
             'uuid': 'CE0E2EF20E2ED36F'
         }
 
+    @staticmethod
+    def jmicron_drive():
         # 2.5″ Western Digital 320Gb (JMicron)
-        self.__jmicron_drive = {
+        return {
             'uuid': '01D8C1D0373750C0',
             'name': 'Western Digital (JMicron)'
         }
-
-    @property
-    def wd_drive(self):
-        return self.__wd_drive
-
-    @property
-    def hitachi_drive(self):
-        return self.__hitachi_drive
-
-    @property
-    def dell_3576_drive(self):
-        return self.__dell_3576_drive
-
-    @property
-    def jmicron_drive(self):
-        return self.__jmicron_drive
