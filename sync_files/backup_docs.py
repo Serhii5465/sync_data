30de1f2cf7a89f14c6aaabdd0e6e6da6497bf2f5
raisnet <raisnet213@gmail.com>
2022-09-17 19:19:34 +0300

    Script reworked

--- sync_files/backup_docs.py
+++ sync_files/backup_docs.py
@@ -1,72 +1,73 @@
 import os
 import sys
-from pathlib import Path
-from src import date, log, bash_proc
+from src.data.rclone_conf import RcloneConf
+from src.bash_process import BashProcess
+from src.date import Date
+from src.log import Log
 
+class BackupDocuments:
 
-def check_files(*list_files):
-    """
-    This function checks if list full.
-    Otherwise, if at least one of the elements in the array missing,
-    the script terminates
-    :param list_files: array of verifiable files
-    """
+    def __init__(self) -> None:
+        self.__rclone = RcloneConf()
+        self.check_files(self.__rclone.conf, self.__rclone.sync_dir, self.__rclone.prog_dir)
+        self.__logs_dir = Log.get_logs_dir('rclone', add_subfolder=False)
 
-    for item in list_files:
-        if not os.path.exists(item):
-            msg = item + " doesn't exists"
-            sys.exit(msg)
-            
+    @property
+    def rclone(self):
+        return self.__rclone
 
-def upload_to_gdrive(rclone_dir, sync_dir, logs_dir):
-    """
-    Uploads files from local folder to Google Drive.
-    Under normal conditions the process finishes work and has an exit code of 0.
-    Otherwise the process has an exit code different from 0 and the script finishes terminates
-    :param rclone_dir: full path to Rclone utility
-    :param sync_dir: synchronized folder
-    :param logs_dir: directory log file
-    """
+    @property
+    def logs_dir(self):
+        return self.__logs_dir
 
-    """
-    Converting Unix-like path to Windows form by using Cygpath.exe utility.
-    Convertion runs in separate process.
-    """
-    win_style_path_sync_dir = bash_proc.get_cmd_output(['cygpath', '--windows', sync_dir])
-    win_style_path_logs_dir = bash_proc.get_cmd_output(['cygpath', '--windows', logs_dir])
+    def check_files(self, *list_files):
+        """
+        This function checks if list full.
+        Otherwise, if at least one of the elements in the array missing,
+        the script terminates
+        :param list_files: array of verifiable files
+        """
 
-    date_now = date.get_time_now()
+        for item in list_files:
+            if not os.path.exists(item):
+                msg = item + " doesn't exists"
+                sys.exit(msg)
 
-    # Process syncing folder with Gdrive
-    out = bash_proc.run_cmd([rclone_dir + '/rclone.exe', 
-                        'sync', 
-                        '--progress', 
-                        '--verbose',
-                        win_style_path_sync_dir.stdout.strip('\n'),
-                        'google-drive:',
-                        '--log-file=' + win_style_path_logs_dir.stdout.strip('\n') + '/' + date_now +'.log'])
-    
-    if out.returncode != 0:
-        sys.exit('Error\nCheck logs')
-    else:
-        print('\nGood')
+    def upload_to_gdrive(self):
+        """
+        Uploads files from local folder to Google Drive.
+        Under normal conditions the process finishes work and has an exit code of 0.
+        Otherwise the process has an exit code different from 0 and the script finishes terminates
+        :param rclone_dir: full path to Rclone utility
+        :param sync_dir: synchronized folder
+        :param logs_dir: directory log file
+        """
 
+        """
+        Converting Unix-like path to Windows form by using Cygpath.exe utility.
+        Convertion runs in separate process.
+        """
+        win_style_path_sync_dir = BashProcess.get_cmd_output(['cygpath', '--windows', self.__rclone.sync_dir])
+        win_style_path_logs_dir = BashProcess.get_cmd_output(['cygpath', '--windows', self.logs_dir])
+
+        date_now = Date.get_time_now()
+
+        # Process syncing folder with Gdrive
+        out = BashProcess.run_cmd([self.__rclone.prog_dir + '/rclone.exe',
+                                 'sync',
+                                 '--progress',
+                                 '--verbose',
+                                 win_style_path_sync_dir.stdout.strip('\n'),
+                                 'google-drive:',
+                                 '--log-file=' + win_style_path_logs_dir.stdout.strip('\n') + '/' + date_now + '.log'])
+
+        if out.returncode != 0:
+            sys.exit('Error\nCheck logs')
+        else:
+            print('\nGood')
 
 def main():
-    create_subfolder = False
-    # Creates log folder and retuning full path in variable
-    logs_dir = log.get_logs_dir('rclone', create_subfolder)
-
-    #Configuration file that stores all the data for the connection Rclone utility for my Google Drive
-    rclone_config = str(Path.home()) + '/.config/rclone/rclone.conf'
-
-    #Full path where located Rclone
-    rclone_path_dir = '/cygdrive/c/portable/rclone'
-
-    #Synchronized folder
-    sync_dir = '/cygdrive/d/documents'
-
-    check_files(rclone_config, rclone_path_dir, sync_dir)
-    upload_to_gdrive(rclone_path_dir, sync_dir, logs_dir)
+    doc = BackupDocuments()
+    doc.upload_to_gdrive()
 
 main()
--- sync_files/sync_music.py
+++ sync_files/sync_music.py
@@ -1,186 +1,184 @@
 import os
 import sys
 import logging
-from src import bash_proc, log, date
+from src.data.hdd_info import HddInfo
+from src.bash_process import BashProcess
+from src.data.adbpaths import AdbPaths
+from src.date import Date
+from src.log import Log
 
 
-def get_form_out_cmd(arg):
-    """
-    Execution command in separate process and returning output stdout
-    without separator of new line
-    @param arg: List of command of with arguments
-    @return: Formatted output
-    """
-    return bash_proc.get_cmd_output(arg).stdout.strip('\n')
+class SyncMusic:
 
+    def __init__(self):
+        self.__bash_proc = BashProcess()
+        self.get_stat_dev()
+        self.__hdd_info = HddInfo()
+        self.__adb_paths = AdbPaths()
+        self.__logger = self.get_logger()
 
-def upload(win_src_dir, root_unix_src_dir, root_dest_dir, sync_dir):
-    """
-    Preparing data and uploading them to Android device using ADB utility.
-    Function verifies, if exist remote folder on smartphone and
-    performs manipulation with files.
-    If folder-receiver does not exist, she will be created and will be pushed files.
-    If folder-receiver. exist, but she is empty, will be pushed files.
-    if the receiver folder exists and is not empty, will be created instance of logger, all files and folders that are
-    missing on the transmitter side will be deleted and new ones will be pushed.
-    @param win_src_dir: Full path to folder-transmitter in Windows style format
-    @param root_unix_src_dir: Full path to root-folder in UNIX style format, where is located folder-transmitter
-    @param root_dest_dir: Full path to root-folder, where is located folder-receiver
-    @param sync_dir: Name of folder-transmitter
-    """
+    @property
+    def hdd_info(self):
+        return self.__hdd_info
 
-    # if directory is exist, command will be returned 0, otherwise 1
-    cmd_is_exist_dir = ['adb', 'shell', '[ -d ' + root_dest_dir + sync_dir + ' ] && echo 0 || echo 1']
-    # if directory is empty, command will be returned 1, otherwise 0
-    cmd_is_empty_dir = ['adb', 'shell',
-                        'find ' + root_dest_dir + sync_dir + ' -mindepth 1 -maxdepth 1 | read && echo 0 || echo 1']  # 0 - not empty, 1 - empty
+    @property
+    def bash_proc(self):
+        return self.__bash_proc
 
-    is_exist = get_form_out_cmd(cmd_is_exist_dir)
-    is_empty = get_form_out_cmd(cmd_is_empty_dir)
+    @property
+    def adb_paths(self):
+        return self.__adb_paths
 
-    cmd_adb_push = ['adb', 'push', win_src_dir, root_dest_dir]
-    cmd_adb_sync = ['adb', 'push', '--sync', win_src_dir, root_dest_dir]
+    @property
+    def logger(self):
+        return self.__logger
 
-    # Initialization of logger
-    logger = get_logger()
+    def get_stat_dev(self):
+        """
+        Verifying connection of USB device by his serial number.
+        """
+        serial_no = '293290c6'
+        cmd_state = ['adb', 'devices']
+        out = self.__bash_proc.get_form_out_cmd(cmd_state)
 
-    if is_exist == '1' and is_empty == '1':
-        # adb push
-        print('Music folder not exist')
-        log_stat_files(get_diff_files(root_dest_dir, sync_dir, root_unix_src_dir), logger, 'Downloadable file: ')
-        bash_proc.run_cmd(cmd_adb_push)
+        if out.find(serial_no) == -1:
+            sys.exit('Device not connected')
 
-    if is_exist == '0' and is_empty == '1':
-        # adb push
-        print('Music folder exist,but empty')
-        log_stat_files(get_diff_files(root_dest_dir, sync_dir, root_unix_src_dir), logger, 'Downloadable file: ')
-        bash_proc.run_cmd(cmd_adb_push)
+    def is_exist_rem_dir(self):
+        # if directory is exist, command will be returned 0, otherwise 1
+        cmd_is_exist_dir = ['adb', 'shell', '[ -d ' + self.adb_paths.root_dest_dir
+                            + self.adb_paths.sync_dir + ' ] && echo 0 || echo 1']
+        # if directory is empty, command will be returned 1, otherwise 0
+        cmd_is_empty_dir = ['adb', 'shell',
+                            'find ' + self.adb_paths.root_dest_dir + self.adb_paths.sync_dir +
+                            ' -mindepth 1 -maxdepth 1 | '
+                            'read && echo 0 || echo 1']  # 0 - not empty, 1 - empty
 
-    if is_exist == '0' and is_empty == '0':
-        # adb push --sync
-        print('Music folder exist and not empty')
+        is_exist = BashProcess.get_form_out_cmd(cmd_is_exist_dir)
+        is_empty = BashProcess.get_form_out_cmd(cmd_is_empty_dir)
 
-        is_upload = False
-        delete_remt_files(get_diff_files(root_dest_dir, sync_dir, root_unix_src_dir, is_upload),
-                          root_dest_dir,
-                          sync_dir,
-                          logger)
+        return is_exist, is_empty
 
-        log_stat_files(get_diff_files(root_dest_dir, sync_dir, root_unix_src_dir), logger, 'Downloadable file: ')
+    def prep_transf(self, is_exist, is_empty):
 
-        print('Uploading files')
-        bash_proc.run_cmd(cmd_adb_sync)
+        cmd_adb_push = ['adb', 'push', '', '']
 
+        list_loc_files = self.get_loc_files()
 
-def delete_remt_files(list_miss_files, root_dest_dir, sync_dir, logger):
-    """
-    Deleting missing files on the local PC from the Android.
-    @param list_miss_files: An array of files that are missing on the sender side.
-    @param root_dest_dir: Full path to root-folder, where is located folder-receiver.
-    @param sync_dir: Name of folder-transmitter.
-    @param logger: Instance of logger.
-    """
-    if len(list_miss_files) != 0:
-        cmd_del_file = ['adb', 'shell', 'rm', '']
-        print('Removing files')
-        for i in list_miss_files:
-            print(i)
-            logger.info('Deletable file: ' + i)
-            cmd_del_file[len(cmd_del_file) - 1] = i
-            out = bash_proc.get_cmd_output(cmd_del_file)
-            if out.returncode != 0:
-                print(out.stderr)
-                logger.error(out.stderr)
+        if is_exist == '1' and is_empty == '1':
+            # adb push
+            print('Music folder not exist')
+            cmd_adb_mkdir = ['adb', 'shell', 'mkdir', self.adb_paths.root_dest_dir + self.adb_paths.sync_dir]
+            BashProcess.run_cmd(cmd_adb_mkdir)
+            self.upload(cmd_adb_push, list_loc_files)
 
-        # Utility 'find' removing empty folders
-        cmd_del_empt_dir = ['adb', 'shell', 'find ' + root_dest_dir + sync_dir + ' -type d -delete']
-        str_empty_dirs = get_form_out_cmd(cmd_del_empt_dir)
-        if str_empty_dirs != '':
-            print('Removing empty directories')
-            list_empty_dirs = str_empty_dirs.split('\n')
-            for i in list_empty_dirs:
+        if is_exist == '0' and is_empty == '1':
+            # adb push
+            print('Music folder exist,but empty')
+            self.upload(cmd_adb_push, list_loc_files)
+
+        if is_exist == '0' and is_empty == '0':
+            # adb push --sync
+            print('Music folder exist and not empty')
+            list_rem_files = self.get_rem_files()
+
+            list_del_files = ['"' + self.adb_paths.root_dest_dir + i +
+                              '"' for i in list(set(list_rem_files) -
+                                                set(list_loc_files))]
+
+            list_upl_files = list(set(list_loc_files) - set(list_rem_files))
+
+            self.delete_remt_files(list_del_files)
+            self.upload(cmd_adb_push, list_upl_files)
+
+    def delete_remt_files(self, files):
+        if len(files) != 0:
+            cmd_del_file = ['adb', 'shell', 'rm', '']
+            msg = 'Removing files...'
+            print(msg)
+            self.logger.info(msg)
+            for i in files:
                 print(i)
-                logger.info('Deletable directory: ' + i)
+                self.__logger.info('Deletable file: ' + i)
+                cmd_del_file[len(cmd_del_file) - 1] = i
+                out = BashProcess.get_cmd_output(cmd_del_file)
+                if out.returncode != 0:
+                    print(out.stderr)
+                    self.__logger.error(out.stderr)
 
+            # Utility 'find' removing empty folders
+            cmd_del_empt_dir = ['adb', 'shell',
+                                'find ' + self.adb_paths.root_dest_dir
+                                + self.adb_paths.sync_dir + ' -type d -delete']
 
-def get_diff_files(root_dest_dir, sync_dir, root_unix_src_dir, is_upload=True):
-    """
-    Finds and compares files on local PC and Android device for further download (update) or deletion.
-    @param root_dest_dir: Full path to root-folder, where is located folder-receiver.
-    @param sync_dir: Name of folder-transmitter.
-    @param root_unix_src_dir: Full path to root-folder in UNIX style format, where is located folder-transmitter
-    @param is_upload: Indicates, which array will be returned. If true, function returned list of files, which
-    be uploaded. Otherwise, returns list of files, which will be removed from Android device.
-    @return:
-    """
-    # Getting list of files on Android device
-    cmd_adb_get_rem_files = ['adb', 'shell', 'cd ' + root_dest_dir + ' && find ' + sync_dir + ' -type f']
-    list_rem_files = get_form_out_cmd(cmd_adb_get_rem_files).split('\n')
+            str_empty_dirs = BashProcess.get_form_out_cmd(cmd_del_empt_dir)
+            if str_empty_dirs != '':
+                print('Removing empty directories')
+                list_empty_dirs = str_empty_dirs.split('\n')
+                for i in list_empty_dirs:
+                    print(i)
+                    self.__logger.info('Deletable directory: ' + i)
 
-    # Getting list of files on PC
-    os.chdir(root_unix_src_dir)
-    cmd_get_loc_files = ['find', sync_dir, '-type', 'f']
-    list_loc_files = get_form_out_cmd(cmd_get_loc_files).split('\n')
+    def get_loc_files(self):
+        os.chdir(self.adb_paths.root_unix_src_dir)
+        cmd_get_loc_files = ['find', self.adb_paths.sync_dir, '-type', 'f']
+        list_loc_files = BashProcess.get_form_out_cmd(cmd_get_loc_files).split('\n')
+        return list_loc_files
 
-    if not is_upload:
-        # List of full paths to files, which are missing on Android
-        return ['"' + root_dest_dir + i + '"' for i in list(set(list_rem_files) - set(list_loc_files))]
-    else:
-        # List of full paths to files, which are will be uploaded from PC to Android device
-        return ['"' + root_unix_src_dir + i + '"' for i in list(set(list_loc_files) - set(list_rem_files))]
+    def get_rem_files(self):
+        cmd_adb_get_rem_files = ['adb', 'shell', 'cd '
+                                 + self.adb_paths.root_dest_dir
+                                 + ' && find '
+                                 + self.adb_paths.sync_dir
+                                 + ' -type f']
 
+        list_rem_files = BashProcess.get_form_out_cmd(cmd_adb_get_rem_files).split('\n')
+        return list_rem_files
 
-def log_stat_files(list_upl_files, logger, msg):
-    """
-    The function logs the full path to the file, which will be uploaded.
-    @param list_upl_files: List of uploading files.
-    @param logger: Instance of logger.
-    @param msg: Message status of file.
-    """
-    for i in list_upl_files:
-        logger.info(msg + i)
+    def upload(self, command, loc_files):
+        msg = 'Uploading files...'
+        print(msg)
+        self.logger.info(msg)
+        cmd_conv_path = ['cygpath.exe', '--windows', '']
 
+        for idx, val in enumerate(loc_files):
+            command[len(command) - 1] = self.adb_paths.root_dest_dir + val
+            val = self.adb_paths.root_unix_src_dir + val
+            cmd_conv_path[len(cmd_conv_path) - 1] = val
+            val = BashProcess.get_form_out_cmd(cmd_conv_path)
+            command[len(command) - 2] = val
+            out = BashProcess.get_cmd_output(command)
 
-def get_stat_dev():
-    """
-    Verifying connection of USB device by his serial number.
-    """
-    serial_no = '293290c6'
-    cmd_state = ['adb', 'devices']
-    out = get_form_out_cmd(cmd_state)
+            if out.returncode == 0:
+                form_out = out.stdout.strip('\n')
+                print(form_out)
+                self.logger.info(form_out)
+            else:
+                form_err = out.stderr.strip('\n')
+                print(form_err)
+                self.logger.error(form_err)
 
-    if out.find(serial_no) == -1:
-        sys.exit('Device not connected')
+    def get_logger(self):
+        """
+        Creating file of logging and Logger object with custom preset.
+        @return: instance of logger
+        """
+        create_subfolder = False
+        date_now = Date.get_time_now()
+        path_logs_dir = Log.get_logs_dir('adb_sync_Redmi-Note-9-Pro', create_subfolder)
+        full_path_log_file = path_logs_dir + '/' + date_now + '.log'
 
+        log_format = "%(levelname)s %(asctime)s - %(message)s"
+        logging.basicConfig(filename=full_path_log_file,
+                            level=logging.INFO,
+                            format=log_format)
 
-def get_logger():
-    """
-    Creating file of logging and Logger object with custom preset.
-    @return: instance of logger
-    """
-    create_subfolder = False
-    date_now = date.get_time_now()
-    path_logs_dir = log.get_logs_dir('adb_sync_Redmi-Note-9-Pro', create_subfolder)
-    full_path_log_file = path_logs_dir + '/' + date_now + '.log'
-
-    log_format = "%(levelname)s %(asctime)s - %(message)s"
-    logging.basicConfig(filename=full_path_log_file,
-                        level=logging.INFO,
-                        format=log_format)
-
-    return logging.getLogger()
-
+        return logging.getLogger()
 
 def main():
-    get_stat_dev()
+    sync_music = SyncMusic()
 
-    sync_dir = 'Music/'
-    root_unix_src_dir = '/cygdrive/d/media/'
-    full_path_unix_src_root = root_unix_src_dir + sync_dir
-    win_src_dir = bash_proc.get_cmd_output(['cygpath.exe', '--windows', full_path_unix_src_root]).stdout.strip('\n')
-
-    root_dest_dir = '/storage/self/primary/'
-
-    upload(win_src_dir, root_unix_src_dir, root_dest_dir, sync_dir)
+    is_exist, is_empty = sync_music.is_exist_rem_dir()
+    sync_music.prep_transf(is_exist, is_empty)
 
 main()
