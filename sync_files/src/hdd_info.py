87dea9f10b95b5cf3518bd6b610b4bbd3e631d5d
raisnet <raisnet213@gmail.com>
2022-09-28 11:43:06 +0300

    Added class for storing info about HDDs and Google style docstrings

--- sync_files/src/hdd_info.py
+++ sync_files/src/hdd_info.py
@@ -1,29 +1,49 @@
-def wd_drive():
-    # Wester Digital 1Tb (Maiwo External Enclosure)
-    return {
-        'uuid': '088C11A28C118AF8',
-        'name': 'Wester Digital (Maiwo)'
-    }
+from typing import Dict
 
 
-def hitachi_drive():
-    # Hitachi 500Gb (Maiwo External Enclosure)
-    return {
-        'uuid': 'B0F014FAF014C90C',
-        'name': 'Hitachi (Maiwo)'
-    }
+class HDDInfo:
+    """
+    Class represents information about HDD: UUID of partition and name model drive.
+    """
+    def __init__(self) -> None:
+        """
+        Initializes dicts with value of UUID and name model of HDD.
+        """
+        #: Wester Digital 1Tb (Maiwo External Enclosure)
+        self.__wd_drive = {
+            'uuid': '088C11A28C118AF8',
+            'name': 'Wester Digital (Maiwo)'
+        }
 
+        #: Hitachi 500Gb (Maiwo External Enclosure)
+        self.__hitachi_drive = {
+            'uuid': 'B0F014FAF014C90C',
+            'name': 'Hitachi (Maiwo)'
+        }
 
-def dell_3576_drive():
-    # Wester Digital 1Tb (Dell Inspiron 15 3576)
-    return {
-        'uuid': 'CE0E2EF20E2ED36F'
-    }
+        #: Wester Digital 1Tb (Dell Inspiron 15 3576)
+        self.__dell_3576_drive = {
+            'uuid': 'CE0E2EF20E2ED36F'
+        }
 
+        #: 2.5″ Western Digital 320Gb (JMicron)
+        self.__jmicron_drive = {
+            'uuid': '01D8C1D0373750C0',
+            'name': 'Western Digital (JMicron)'
+        }
 
-def jmicron_drive():
-    # 2.5″ Western Digital 320Gb (JMicron)
-    return {
-        'uuid': '01D8C1D0373750C0',
-        'name': 'Western Digital (JMicron)'
-    }
+    @property
+    def wd_drive(self) -> Dict[str, str]:
+        return self.__wd_drive
+
+    @property
+    def hitachi_drive(self) -> Dict[str, str]:
+        return self.__hitachi_drive
+
+    @property
+    def dell_3576_drive(self) -> Dict[str, str]:
+        return self.__dell_3576_drive
+
+    @property
+    def jmicron_drive(self) -> Dict[str, str]:
+        return self.__jmicron_drive
