a6ad893e15f030a045a6739e4ffb472b98d298ad
raisnet <raisnet213@gmail.com>
2023-12-03 15:45:46 +0200

    UUID changed

--- src/hdd_info.py
+++ src/hdd_info.py
@@ -3,28 +3,28 @@ from typing import Dict
 #: Wester Digital 1Tb (Maiwo External Enclosure)
 def WD_DRIVE() -> Dict[str, str]:
     return {
-        'uuid': '647EF4017EF3CA36',
+        'uuid': '01DA25E4223300B0',
         'name': 'Wester Digital (Maiwo)'
     }
 
 #: Hitachi 500Gb (Maiwo External Enclosure)
 def HITACHI_DRIVE() -> Dict[str, str]:
     return {
-        'uuid': '969A62AA9A628719',
+        'uuid': '01DA25E89FDC5B70',
         'name': 'Hitachi (Maiwo)'
     }
 
 #: 2.5″ Western Digital 320Gb (JMicron)
 def JMICRON_DRIVE() -> Dict[str, str]:
     return {
-            'uuid': 'CC1C8A351C8A1B1A',
+            'uuid': 'C8CADB06CADAEFA4',
             'name': 'Western Digital (JMicron)'
     }
 
 #: Wester Digital 1Tb (MSI GF63)
 def MSI_GF63_DRIVE() -> Dict[str, str]:
     return {
-        'uuid': '122C7D8D2C7D6D1B'
+        'uuid': 'CA607FCC607FBDAF'
     }
 
 def DELL_INSPIRON_DRIVE() -> Dict[str, str]:
