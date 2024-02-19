031d260b743896cf590ceb9eb7a14107d78dbc58
Serhii5465 <serhiiartiukh5@gmail.com>
2024-02-19 14:38:31 +0200

    Upd

--- src/hdd_info.py
+++ src/hdd_info.py
@@ -3,26 +3,26 @@ from typing import Dict
 #: Wester Digital 1Tb (Maiwo External Enclosure)
 def WD_DRIVE() -> Dict[str, str]:
     return {
-        'uuid': '01DA25E4223300B0',
+        'uuid': '44A6D12EA6D1216A',
         'name': 'Wester Digital (Maiwo)'
     }
 
 #: Hitachi 500Gb (Maiwo External Enclosure)
 def HITACHI_DRIVE() -> Dict[str, str]:
     return {
-        'uuid': '01DA25E89FDC5B70',
+        'uuid': '6624859F248572BB',
         'name': 'Hitachi (Maiwo)'
     }
 
 #: 2.5″ Western Digital 320Gb (JMicron)
 def JMICRON_DRIVE() -> Dict[str, str]:
     return {
-        'uuid': 'FE821EBE821E7B7B',
+        'uuid': 'B82CE9172CE8D204',
         'name': 'Western Digital (JMicron)'
     }
 
 #: Wester Digital 1Tb (MSI GF63)
 def MSI_GF63_DRIVE() -> Dict[str, str]:
     return {
-        'uuid': '01DA431E36406280'
+        'uuid': 'A478EEBD78EE8CFC'
     }
