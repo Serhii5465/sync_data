6ab653ef3f9d73f4f15ad04b6f07fac0b00ed296
Serhii5465 <serhiiartiukh5@gmail.com>
2024-04-12 11:28:02 +0300

    constants.py - Code refactoring

--- src/constants.py
+++ src/constants.py
@@ -4,39 +4,35 @@ from typing import Dict
 def EXT_DRIVE_1() -> Dict[str, str]:
     return {
         'uuid': '94B42285B4226A46',
-        'name': 'Wester Digital (Maiwo)'
+        'label': 'Wester Digital (Maiwo)'
     }
 
 #: Hitachi 500Gb (Maiwo External Enclosure)
 def EXT_DRIVE_2() -> Dict[str, str]:
     return {
         'uuid': '56384C06384BE419',
-        'name': 'Hitachi (Maiwo)'
+        'label': 'Hitachi (Maiwo)'
     }
 
 #: 2.5″ Wester Digital Mobile 320Gb (Gembird enclosure)
 def EXT_DRIVE_3() -> Dict[str, str]:
     return {
         'uuid': '845A6F615A6F4ECC',
-        'name': 'Wester Digital (Gembird)'
+        'label': 'Wester Digital (Gembird)'
     }
 
-# #: Samsung SSD 500Gb (MSI GF63)
-# def MSI_GF63_SRC_DRIVE_1() -> Dict[str, str]:
-#     return {
-#         'uuid': '01DA36B803C75B50',
-#         'sync_dirs': ['configs' , 'documents', 'media']
-#     }
-
-# #: Kingston SSD 500Gb (MSI GF63)
-# def MSI_GF63_SRC_DRIVE_2() -> Dict[str, str]:
-#     return {
-#         'uuid': '8E76883376881DD9',
-#         'sync_dirs': ['backups' , 'installers', 'vm']
-#     }
-
-def MSI_GF63_SRC_DRIVE() -> Dict[str, str]:
+def MSI_GF63_SRC_DRIVE() -> Dict[str, any]:
     return {
         'uuid': '347E0E947E0E4F54',
-        'sync_dirs': ['configs' , 'documents', 'media', 'backups', 'installers', 'vm']
+        'sync_dirs': ['shared', 'configs' , 'documents', 'media', 'backups', 'installers', 'vm'],
+        'log_name' : 'backup_msi_gf63',
+        'name_dest_dir' : 'msi_gf63_files'
+    }
+
+def DELL_INSPIRON_3576_SRC_DRIVE() -> Dict[str, any]:
+    return {
+        'uuid': '345837F85837B806',
+        'sync_dirs': ['configs' , 'backups'],
+        'log_name' : 'backup_dell_inspiron_3576',
+        'name_dest_dir' : 'dell_inspiron_3576_files'
     }
